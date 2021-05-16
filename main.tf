dac99c616f6f7e509e9bb7e60736bfc034025115
Joe Buhr <jtb75@users.noreply.github.com>
2021-05-16 13:59:36 -0500

    .

--- main.tf
+++ main.tf
@@ -2,11 +2,9 @@ provider "aws" {
   region = "us-east-1"
 }
 
-/*
 module "utility" {
   key_pair = var.key_pair
   ec2_region = var.ec2_region
   admin_ip = var.admin_ip
   source = "./utility"
 }
-*/
