e0dc43df2fd1e6f270915a87ceab60aa17f2a5cb
Joe Buhr <jtb75@users.noreply.github.com>
2021-05-16 15:16:12 -0500

    .

--- main.tf
+++ main.tf
@@ -8,6 +8,12 @@ module "utility" {
   admin_ip = var.admin_ip
   source = "./utility"
 }
+output "Utility_Instance_SSH_Access" {
+  value = module.utility.utility_instance_public_ip
+}
+output "Web_Instance_SSH_Access" {
+  value =module.utility.web_instance_public_ip
+}
 
 /*
 module "tpot" {
@@ -15,5 +21,14 @@ module "tpot" {
   ec2_region = var.ec2_region
   admin_ip = var.admin_ip
   source = "./tpot"
+}
+output "T-Pot_Admin_UI" {
+  value = module.tpot.Admin_UI
+}
+output "T-Pot_SSH_Access" {
+  value = module.tpot.SSH_Access
+}
+output "T-Pot_Web_UI" {
+  value = module.tpot.Web_UI
 }
 */
