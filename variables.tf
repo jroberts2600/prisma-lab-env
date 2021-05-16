9d63eb6d861cc1501c62d1f398d02f19ec1b3e75
Joe Buhr <jtb75@users.noreply.github.com>
2021-05-16 15:00:51 -0500

    .

--- variables.tf
+++ variables.tf
@@ -1,3 +1,11 @@
+variable "AWS_ACCESS_KEY_ID" {
+  type = string
+}
+
+variable "AWS_SECRET_ACCESS_KEY" {
+  type = string
+}
+
 variable "key_pair" {
   description = "Key pair to be used on ec2 instances"
   default = "nv-pan"
