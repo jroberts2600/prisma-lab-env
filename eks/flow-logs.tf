11ec865b7d060693f9b362bf06c33d95fb50b8de
Joe Buhr <jtb75@users.noreply.github.com>
2021-05-16 19:42:05 -0500

    .

--- eks/flow-logs.tf
+++ eks/flow-logs.tf
@@ -1,11 +1,11 @@
 resource "aws_flow_log" "pc_flow" {
   iam_role_arn    = var.flow_role_arn
-  log_destination = aws_cloudwatch_log_group.pc_flow_log.arn
+  log_destination = aws_cloudwatch_log_group.eks_flow_log.arn
   traffic_type    = "ALL"
   vpc_id          = module.vpc.vpc_id
 }
 
-resource "aws_cloudwatch_log_group" "pc_flow_log" {
-  name = "utility_flow_log"
+resource "aws_cloudwatch_log_group" "eks_flow_log" {
+  name = "eks_flow_log"
   retention_in_days = 1
 }
