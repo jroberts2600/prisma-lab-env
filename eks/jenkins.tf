e43ea332c78bdf981082a9eb232b707fb2ef81c8
Joe Buhr <jtb75@users.noreply.github.com>
2021-06-03 14:09:49 -0500

    added jenkins deployment

--- eks/jenkins.tf
+++ eks/jenkins.tf
@@ -3,3 +3,43 @@ resource "kubernetes_namespace" "jenkins" {
     name = "jenkins"
   }
 }
+
+resource "kubernetes_deployment" {
+  metadata {
+    name = "jenkins"
+  }
+  spec {
+    replicas = 1
+    selector {
+      match_labels = {
+        app = "jenkins"
+      }
+    }
+    template {
+      metadata {
+        labels = {
+          app = "jenkins"
+        }
+      }
+      spec {
+        container {
+          image = "jenkins/jenkins:lts-jdk11"
+          name = "jenkins"
+          port {
+            container_port = 80
+          }
+          volume_mount {
+            name = "jenkins-home"
+            mount_path = "/var/jenkins_home"
+          }
+        }
+        volume {
+          name = "jenkins-home"
+          empty_dir {
+            
+          }
+        }
+      }
+    }
+  }
+}
