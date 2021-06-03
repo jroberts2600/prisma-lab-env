592838b4bae57c78899f37bc82c05ed57c494bf0
Joe Buhr <jtb75@users.noreply.github.com>
2021-06-03 12:40:13 -0500

    .

--- eks/kubernetes.tf
+++ eks/kubernetes.tf
@@ -7,8 +7,6 @@
 
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.cluster.endpoint
-  //client_certificate     = data.aws_eks_cluster.client_certificate
-  //client_key             = data.aws_eks_cluster.client_key
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
   exec {
     api_version = "client.authentication.k8s.io/v1alpha1"
@@ -21,3 +19,10 @@ provider "kubernetes" {
     ]
   }
 }
+
+resource "kubernetes_namespace" "sock-shop" {
+  metadata {
+    name = "sock-shop"
+  }
+}
+
