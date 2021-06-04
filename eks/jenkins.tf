ddb314c0c6e30f0ea596a7f7b553b8b164a49500
Joe Buhr <jtb75@users.noreply.github.com>
2021-06-03 19:31:54 -0500

    .

--- eks/jenkins.tf
+++ eks/jenkins.tf
@@ -3,7 +3,7 @@ resource "kubernetes_namespace" "jenkins" {
     name = "jenkins"
   }
 }
-
+/*
 resource "kubernetes_deployment" jenkins {
   metadata {
     name = "jenkins"
@@ -47,7 +47,7 @@ resource "kubernetes_deployment" jenkins {
     }
   }
 }
-/*
+
 resource "kubernetes_service" "jenkins" {
   metadata {
     name      = "jenkins"
