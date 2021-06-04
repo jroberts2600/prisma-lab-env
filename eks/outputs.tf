4037e6f52bfe8bfb1590fb2bc4878824243b5bce
Joe Buhr <jtb75@users.noreply.github.com>
2021-06-04 12:53:26 -0500

    .

--- eks/outputs.tf
+++ eks/outputs.tf
@@ -35,5 +35,5 @@ output "cluster_connect" {
 
 output "sock-shop_url" {
   description = "Sock Shop Application"
-  value       = kubernetes_service.front_end.status.0.loadBalancer.0.ingress.0.hostname
+  value       = kubernetes_service.front_end.status.0.load_balancer.0.ingress.0.hostname
 }
