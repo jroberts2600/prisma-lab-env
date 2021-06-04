918960448624636c0d5b420e0596915f6cac22db
Joe Buhr <jtb75@users.noreply.github.com>
2021-06-04 15:41:56 -0500

    Removed Test ssh string output.

--- utility/outputs.tf
+++ utility/outputs.tf
@@ -5,7 +5,3 @@ output "utility_instance_public_ip" {
 output "web_instance_public_ip" {
   value = "ssh -i ${var.key_pair}.pem ubuntu@${aws_instance.web_instance.public_dns}"
 }
-
-output "test_instance_public_ip" {
-  value = "ssh -i ${var.key_pair}.pem ubuntu@${aws_instance.test_instance.public_dns}"
-}
