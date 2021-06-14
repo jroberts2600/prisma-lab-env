48852566dadfa8e73599223e3188afaea4dcf2e6
Joe Buhr <jtb75@users.noreply.github.com>
2021-06-13 21:27:58 -0500

    .

--- sock-shop/storage.tf
+++ sock-shop/storage.tf
@@ -6,3 +6,24 @@ resource "aws_ebs_volume" "sock-shop" {
     App = "Sock-Shop"
   }
 }
+
+resource "kubernetes_persistent_volume" "pv" {
+  metadata {
+    name = "pv"
+  }
+
+  spec {
+    capacity = {
+      storage = "5Gi"
+    }
+
+    access_modes                     = ["ReadWriteOnce"]
+    persistent_volume_reclaim_policy = "Retain"
+    storage_class_name               = "gp2-retain"
+    persistent_volume_source {
+      aws_elastic_block_store {
+        volume_id       = aws_ebs_volume.sock-shop.id
+      }
+    }
+  }
+}
