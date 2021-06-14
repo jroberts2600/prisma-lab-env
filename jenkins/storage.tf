49a842df5a708eb562572addf80d6a3b5e5e4823
Joe Buhr <jtb75@users.noreply.github.com>
2021-06-14 08:26:21 -0500

    .

--- jenkins/storage.tf
+++ jenkins/storage.tf
@@ -59,7 +59,7 @@ resource "kubernetes_storage_class" "ebs_sc" {
     name = "ebs-sc"
   }
 
-  parameter {}
+  storage_provisioner = ebs.csi.aws.com
 
   mount_options       = ["debug"]
   volume_binding_mode = "Immediate"
@@ -67,7 +67,7 @@ resource "kubernetes_storage_class" "ebs_sc" {
 
 resource "kubernetes_persistent_volume_claim" "ebs_claim" {
   metadata {
-    name = "ebs-claim"
+    name = "jenkins-pvc"
   }
 
   spec {
