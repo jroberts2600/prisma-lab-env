7443e6e526194a61f3ccf0459e6a013e0f8d869b
Joe Buhr <jtb75@users.noreply.github.com>
2021-06-14 07:26:25 -0500

    .

--- jenkins/jenkins.tf
+++ jenkins/jenkins.tf
@@ -4,7 +4,6 @@ resource "kubernetes_namespace" "jenkins" {
   }
 }
 
-/*
 resource "kubernetes_deployment" jenkins {
   metadata {
     name = "jenkins"
@@ -22,6 +21,7 @@ resource "kubernetes_deployment" jenkins {
       }
     }
 
+/*
     volume {
       name = "jenkins-home"
 
@@ -29,6 +29,7 @@ resource "kubernetes_deployment" jenkins {
         claim_name = "jenkins-pvc"
       }
     }
+*/
 
     template {
       metadata {
@@ -36,19 +37,19 @@ resource "kubernetes_deployment" jenkins {
           app = "jenkins"
         }
       }
-    }
 
-    spec {
-      container {
-        //image = "jenkins/jenkins:lts-jdk11"
-        image = "public.ecr.aws/q6t7l4t3/jenkins-tf:0.2"
-        name = "jenkins"
-        port {
-          container_port = 8080
-        }
-        volume_mount {
-          name = "jenkins-home"
-          mount_path = "/var/jenkins_home"
+      spec {
+        container {
+          //image = "jenkins/jenkins:lts-jdk11"
+          image = "public.ecr.aws/q6t7l4t3/jenkins-tf:0.2"
+          name = "jenkins"
+          port {
+            container_port = 8080
+          }
+          volume_mount {
+            name = "jenkins-home"
+            mount_path = "/var/jenkins_home"
+          }
         }
       }
     }
@@ -76,65 +77,5 @@ resource "kubernetes_service" "jenkins" {
     }
 
     type = "LoadBalancer"
-  }
-}
-*/
-resource "kubernetes_deployment" "example" {
-  metadata {
-    name = "terraform-example"
-    labels = {
-      test = "MyExampleApp"
-    }
-  }
-
-  spec {
-    replicas = 3
-
-    selector {
-      match_labels = {
-        test = "MyExampleApp"
-      }
-    }
-
-    template {
-      metadata {
-        labels = {
-          test = "MyExampleApp"
-        }
-      }
-
-      spec {
-        container {
-          image = "nginx:1.7.8"
-          name  = "example"
-
-          resources {
-            limits = {
-              cpu    = "0.5"
-              memory = "512Mi"
-            }
-            requests = {
-              cpu    = "250m"
-              memory = "50Mi"
-            }
-          }
-
-          liveness_probe {
-            http_get {
-              path = "/nginx_status"
-              port = 80
-
-              http_header {
-                name  = "X-Custom-Header"
-                value = "Awesome"
-              }
-            }
-
-            initial_delay_seconds = 3
-            period_seconds        = 3
-          }
-        }
-      }
-    }
   }
 }
