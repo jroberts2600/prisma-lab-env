c38f25d6283df8802e98726acc0d1030a9dd6c41
Joe Buhr <jtb75@users.noreply.github.com>
2021-09-01 19:43:02 -0500

    Update flow-logs.tf

--- tpot/flow-logs.tf
+++ tpot/flow-logs.tf
@@ -1,3 +1,4 @@
+/*
 resource "aws_flow_log" "pc_flow" {
   iam_role_arn    = var.flow_role_arn
   log_destination = aws_cloudwatch_log_group.tpot_flow_log.arn
@@ -9,3 +10,4 @@ resource "aws_cloudwatch_log_group" "tpot_flow_log" {
   name = "tpot_flow_log-${random_string.suffix.id}"
   retention_in_days = 1
 }
+*/
