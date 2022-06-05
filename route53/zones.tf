87a8c9b5c27559b4eec52b379423e8e535b511fe
Joe Buhr <joe.buhr@gmail.com>
2022-06-05 13:13:21 -0500

    .

--- route53/zones.tf
+++ route53/zones.tf
@@ -1,3 +1,16 @@
 resource "aws_route53_zone" "primary" {
   name = "foobar.io"
 }
+
+resource "aws_route53_recors" "demo" {
+  zone_id = aws_route53_zone.primary.zone_id
+  name    = "foobar.io"
+  type    = "A"
+  ttl     = "300"
+  records = [
+    "ns-1935.awsdns-49.co.uk.",
+    "ns-381.awsdns-47.com.",
+    "ns-832.awsdns-40.net.",
+    "ns-1275.awsdns-31.org."
+  ]
+}
