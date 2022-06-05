700aa051f77c6965ab3501ee9f74aaa853a8d19e
Joe Buhr <joe.buhr@gmail.com>
2022-06-05 14:40:31 -0500

    .

--- route53/zones.tf
+++ route53/zones.tf
@@ -4,7 +4,7 @@ resource "aws_route53_zone" "primary" {
 
 resource "aws_route53_record" "utility" {
   zone_id = aws_route53_zone.primary.zone_id
-  name    = "utility."aws_route53_zone.name
+  name    = "utility.${aws_route53_zone.name}"
   type    = "A"
   ttl     = "300"
   records = [var.utility_ip]
