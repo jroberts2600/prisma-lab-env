329674de1131ddb62de703ce5ece7da96858a8e5
Joe Buhr <joe.buhr@gmail.com>
2022-06-06 07:58:12 -0500

    .

--- modules/s3/s3.tf
+++ modules/s3/s3.tf
@@ -52,3 +52,13 @@ resource "aws_s3_bucket_logging" "private" {
   target_bucket = aws_s3_bucket.log_bucket.id
   target_prefix = "private-log/"
 }
+
+resource "aws_s3_bucket_public_access_block" "private" {
+  bucket = aws_s3_bucket.private-bucket.id
+
+  block_public_acls   = true
+  block_public_policy = true
+  ignore_public_acls = true
+  restrict_public_buckets = true
+  
+}
