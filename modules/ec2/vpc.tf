b0ed78646db79ef4d6042c5a1177e33cfcdd3607
Joe Buhr <joe.buhr@gmail.com>
2022-06-06 07:38:42 -0500

    .

--- modules/ec2/vpc.tf
+++ modules/ec2/vpc.tf
@@ -112,4 +112,16 @@ resource "aws_security_group" "allow_outbound" {
 resource "aws_default_security_group" "default" {
   vpc_id = aws_vpc.utility.id
 
+    ingress {
+    protocol  = -1
+    from_port = 0
+    to_port   = 0
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+  }
+  
 }
