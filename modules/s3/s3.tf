f3493ba2e8811b9a471dd1f184000930112c6510
Joe Buhr <joe.buhr@gmail.com>
2022-06-06 11:30:25 -0500

    .

--- modules/s3/s3.tf
+++ modules/s3/s3.tf
@@ -14,30 +14,54 @@ resource "aws_s3_bucket_versioning" "marketing" {
   }
 }
 
-resource "aws_s3_bucket" "private-bucket" {
+resource "aws_s3_bucket_logging" "marketing" {
+  bucket = aws_s3_bucket.marketing.id
+
+  target_bucket = aws_s3_bucket.log_bucket.id
+  target_prefix = "marketing-log/"
+}
+
+resource "aws_s3_bucket" "private" {
   bucket = "private-${random_string.suffix.id}"
 }
 
-resource "aws_s3_bucket_acl" "private_bucket_acl" {
-  bucket = aws_s3_bucket.private-bucket.id
+resource "aws_s3_bucket_acl" "private" {
+  bucket = aws_s3_bucket.private.id
   acl    = "private"
 }
 
-resource "aws_s3_bucket_versioning" "private_versioning" {
-  bucket = aws_s3_bucket.private-bucket.id
+resource "aws_s3_bucket_versioning" "private" {
+  bucket = aws_s3_bucket.private.id
   versioning_configuration {
     status = "Enabled"
   }
 }
 
-resource "aws_s3_bucket_server_side_encryption_configuration" "private_encryption" {
-  bucket = aws_s3_bucket.private-bucket.id
+resource "aws_s3_bucket_server_side_encryption_configuration" "private" {
+  bucket = aws_s3_bucket.private.id
   rule {
     apply_server_side_encryption_by_default {
         kms_master_key_id = var.s3_key_arn
         sse_algorithm     = "aws:kms"
     }
   }
+}
+
+resource "aws_s3_bucket_public_access_block" "private" {
+  bucket = aws_s3_bucket.private.id
+
+  block_public_acls   = true
+  block_public_policy = true
+  ignore_public_acls = true
+  restrict_public_buckets = true
+  
+}
+
+resource "aws_s3_bucket_logging" "private" {
+  bucket = aws_s3_bucket.private.id
+
+  target_bucket = aws_s3_bucket.log_bucket.id
+  target_prefix = "private-log/"
 }
 
 resource "aws_s3_bucket" "log_bucket" {
@@ -47,28 +71,4 @@ resource "aws_s3_bucket" "log_bucket" {
 resource "aws_s3_bucket_acl" "log_bucket_acl" {
   bucket = aws_s3_bucket.log_bucket.id
   acl    = "log-delivery-write"
-}
-
-resource "aws_s3_bucket_logging" "marketing" {
-  bucket = aws_s3_bucket.marketing.id
-
-  target_bucket = aws_s3_bucket.log_bucket.id
-  target_prefix = "marketing-log/"
-}
-
-resource "aws_s3_bucket_logging" "private" {
-  bucket = aws_s3_bucket.private-bucket.id
-
-  target_bucket = aws_s3_bucket.log_bucket.id
-  target_prefix = "private-log/"
-}
-
-resource "aws_s3_bucket_public_access_block" "private" {
-  bucket = aws_s3_bucket.private-bucket.id
-
-  block_public_acls   = true
-  block_public_policy = true
-  ignore_public_acls = true
-  restrict_public_buckets = true
-  
 }
